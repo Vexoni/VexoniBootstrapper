12474be43093acf6bf80cfce61fda60896a09693
Aep <86505664+Aeplexi@users.noreply.github.com>
2024-01-07 20:15:16 -0500

    2021

--- BootstrapperClient/BootstrapperClient.cpp
+++ BootstrapperClient/BootstrapperClient.cpp
@@ -1121,7 +1121,7 @@ void BootstrapperClient::DeployComponents(bool isUpdating, bool commitData)
 
 	createDirectory((programDirectory() + _T("2017L")).c_str());
 	files.push_back(std::pair<std::wstring, std::wstring>(_T("RobloxApp.zip"), _T("2017L"))); // 2017L
-
+	files.push_back(std::pair<std::wstring, std::wstring>(_T("RobloxApp2021E.zip"), _T("2021E"))); // 2017L
 	DoDeployComponents(files, isUpdating, commitData);
 }
 
