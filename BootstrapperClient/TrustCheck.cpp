4ca7cb2d88145479c23a0e66d4cf24aba5116cbe
Aep <86505664+Aeplexi@users.noreply.github.com>
2024-03-24 22:40:04 -0400

    2019L

--- BootstrapperClient/TrustCheck.cpp
+++ BootstrapperClient/TrustCheck.cpp
@@ -5,7 +5,7 @@
 
 bool TrustCheck::trustCheck(std::string clientVersion)
 {
-    std::string allowedVersions[] = { "2017L", "2015E", "2021E", "2019M", "2013L" };
+    std::string allowedVersions[] = { "2017L", "2015E", "2021E", "2019L", "2013L" };
     // check if contains
     auto it = std::find(std::begin(allowedVersions), std::end(allowedVersions), clientVersion);
     return it != std::end(allowedVersions);
