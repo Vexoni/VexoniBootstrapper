466c41ca0c5260167fc8dcf540563827754dae17
Aep <86505664+Aeplexi@users.noreply.github.com>
2024-07-10 09:52:40 -0400

    no trusted versions yet because of rewrite

--- BootstrapperClient/TrustCheck.cpp
+++ BootstrapperClient/TrustCheck.cpp
@@ -5,7 +5,7 @@
 
 bool TrustCheck::trustCheck(std::string clientVersion)
 {
-    std::string allowedVersions[] = { "2017M", "2015E", "2021E", "2019L", "2013L"};
+    std::string allowedVersions[] = {};
     // check if contains
     auto it = std::find(std::begin(allowedVersions), std::end(allowedVersions), clientVersion);
     return it != std::end(allowedVersions);
